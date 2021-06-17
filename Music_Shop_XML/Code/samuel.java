621c8db64936987ba70f8bccdbee21267a20ab82
Samuel Sherpa <64248032+SamuelSherpa2020@users.noreply.github.com>
2021-06-17 18:11:06 +0545

    First commit of XML project
    I am really excited to push a good-first XML project.

---
+++ Music_Shop_XML/Code/samuel.java
@@ -0,0 +1,6 @@
+public class samuel {
+    public static void main(String[] args) {
+        System.out.println("my name is samuel sherpa ");
+        System.out.println("I am coming back to java");
+    }
+}
